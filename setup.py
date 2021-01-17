1371eff77b4e638d3125f41777fa44a60f14ac5a
sxc <sxc@localhost.localdomain>
2021-01-17 23:31:04 +0800

    update setup and README

--- setup.py
+++ setup.py
@@ -15,6 +15,10 @@ setuptools.setup(
     url="https://github.com/shengxc/sutil",
     license='Apache License 2.0',
     packages=setuptools.find_packages(exclude=["test"]),
+    install_requires=[
+        "requests>=2.24.0",
+        "aiohttp>=3.7.3"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
